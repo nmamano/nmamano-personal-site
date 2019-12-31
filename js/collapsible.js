6d723def7bd63cdcbfddf61dd2699df903062335
Nil <nil.mamano@gmail.com>
2019-12-31 15:39:36 -0600

    paper summaries

--- js/collapsible.js
+++ js/collapsible.js
@@ -8,8 +8,14 @@ for (i = 0; i < coll.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      this.style.boxShadow = "none";
+      this.style.border= "none";
     } else {
       content.style.display = "block";
+      this.style.boxShadow = "0px 10px 5px #aaaaaa";
+      this.style.border=""
+      this.style.borderTop="1px solid black";
+      this.style.borderBottom="1px solid black";
     }
   });
 }
