5eb3b5bcf9262036931f8129155dde24db294657
Nil <nil.mamano@gmail.com>
2020-01-08 21:43:13 -0600

    bulk stylistic improvements and UI fixes
    faster loading icons, highlight collapsible content with different bg color, square icons on nav bar, fixed cv icon size, white bg for svg pics, nav-bar now spans 100% width, misc. pic now bigger on phones, removed arrow icon, clicking an icon or selecting a paper's title does not open collapsible, recentered footnote, possibly more

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,21 +1,21 @@
 
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("data");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    if (window.getSelection().toString()) return;
+    this.parentElement.classList.toggle("active");
+    var content = this.parentElement.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
-      this.style.boxShadow = "none";
-      this.style.border= "none";
+      this.parentElement.style.boxShadow = "none";
+      this.parentElement.style.border= "none";
     } else {
       content.style.display = "block";
-      this.style.boxShadow = "0px 10px 5px #aaaaaa";
-      this.style.border=""
-      this.style.borderTop="1px solid black";
-      this.style.borderBottom="1px solid black";
+      this.parentElement.style.boxShadow = "0px 10px 5px #aaaaaa";
+      this.parentElement.style.borderTop="1px solid black";
+      this.parentElement.style.borderBottom="1px solid black";
     }
   });
 }
