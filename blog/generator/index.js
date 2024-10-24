7091abedd9dc33ddbfb7ae14a1a89cb74c93816b
nmamano <nil.mamano@gmail.com>
2024-10-24 12:07:42 -0700

    Improved blog html generator.
    Also minor tweaks to last blog post.

--- blog/generator/index.js
+++ blog/generator/index.js
@@ -1,53 +1,117 @@
-var showdown = require('showdown');
-var hljs = require('highlight.js');
+const showdown = require("showdown");
+const hljs = require("highlight.js");
+const fs = require("fs");
+const path = require("path");
 
-//extension source: https://github.com/showdownjs/showdown/issues/577#issuecomment-417181311 
-showdown.extension('highlight', function () {
-return [{
+// Extension source: https://github.com/showdownjs/showdown/issues/577#issuecomment-417181311
+showdown.extension("highlight", () => [
+  {
     type: "output",
-    filter: function (text, converter, options) {
-        var left = "<pre><code\\b[^>]*>",
-            right = "</code></pre>",
-            flags = "g";
-        var replacement = function (wholeMatch, match, left, right) {
-            var lang = (left.match(/class=\"([^ \"]+)/) || [])[1];
-            left = left.slice(0, 18) + 'hljs ' + left.slice(18);
-            match = match
-            .replace(/&amp;/g, '&')
-            .replace(/&lt;/g, '<')
-            .replace(/&gt;/g, '>')
-            if (lang && hljs.getLanguage(lang)) {
-                return left + hljs.highlight(lang, match).value + right;
-            } else {
-                return left + hljs.highlightAuto(match).value + right;
-            }
-        };
-        return showdown.helper.replaceRecursiveRegExp(text, replacement, left, right, flags);
-    }
-}];
-});
+    filter: (text) => {
+      const left = "<pre><code\\b[^>]*>";
+      const right = "</code></pre>";
+      const flags = "g";
 
-var converter = new showdown.Converter({
-extensions: ['highlight']
-});
-converter.setOption('parseImgDimensions', true);
-converter.setOption('simplifiedAutoLink', true);
-converter.setOption('ghCodeBlocks', true);
-// converter.setOption('smoothLivePreview', true);
-converter.setFlavor('github');
+      const replacement = (wholeMatch, match, left, right) => {
+        const lang = (left.match(/class="([^ "]*)/) || [])[1];
+        left = left.slice(0, 18) + "hljs " + left.slice(18);
+        match = match
+          .replace(/&amp;/g, "&")
+          .replace(/&lt;/g, "<")
+          .replace(/&gt;/g, ">");
 
-var fs = require('fs');
-var markdownFilePath = process.argv[2];
-var outputFile = 'autogen.html';
-fs.readFile( markdownFilePath, 'utf8', function(err, text) {
+        if (lang && hljs.getLanguage(lang)) {
+          return left + hljs.highlight(lang, match).value + right;
+        } else {
+          return left + hljs.highlightAuto(match).value + right;
+        }
+      };
+
+      return showdown.helper.replaceRecursiveRegExp(
+        text,
+        replacement,
+        left,
+        right,
+        flags
+      );
+    },
+  },
+]);
+
+const converter = new showdown.Converter({
+  extensions: ["highlight"],
+  parseImgDimensions: true,
+  simplifiedAutoLink: true,
+  ghCodeBlocks: true,
+});
+converter.setFlavor("github");
+
+const markdownFilePath = process.argv[2];
+const outputFile = path.join(
+  path.dirname(markdownFilePath),
+  `${path.basename(markdownFilePath, ".md")}.html`
+);
+
+fs.readFile(markdownFilePath, "utf8", (err, text) => {
+  if (err) throw err;
+
+  let firstLine = text.split("\n")[0].replace(/^#\s*/, "");
+  const outputDir = path.basename(markdownFilePath, ".md");
+  const url = `https://nilmamano.com/blog/${outputDir}/${outputDir}.html`;
+  const thumbnailUrl = `https://nilmamano.com/blog/${outputDir}/thumbnail.png`;
+
+  const prefix = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Nil Mamano | Blog</title>
+  <meta name="person" content="Nil Mamano">
+  <meta name="sortname" content="Mamano, Nil">
+  <meta name="keywords" content="personal site,computer science,blog">
+  <meta name="description" content="${firstLine}">
+
+  <meta property="og:title" content="Nil Mamano | Blog" />
+  <meta property="og:description" content="${firstLine}" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="${url}" />
+  <meta property="og:image" content="${thumbnailUrl}" />
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="../../css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/default.min.css">
+
+</head>
+
+<body class="blogpostpage">
+  <main class="blogpost">
+    <div class="blogreturn">
+      <p>Return to the <a href="../../blog.html">blog's main page</a>.</p>
+    </div>
+    
+    <div id="mdToHtml">
+`;
+
+  const suffix = `    </div>
+
+    <div class="blogreturn">
+      <p>Return to the <a href="../../blog.html">blog's main page</a>.</p>
+    </div>
+  </main>
+
+</body>
+`;
+
+  const genHTML = converter.makeHtml(text);
+  fs.writeFile(outputFile, `${prefix}${genHTML}${suffix}`, (err) => {
     if (err) throw err;
-    var genHTML = converter.makeHtml(text);
-    fs.writeFile(outputFile, genHTML, (err) => {
-        if (err) throw err;
-        console.log('The generated html has been saved');
-      });
+    console.log("The generated HTML has been saved as " + outputFile);
+  });
 });
 
-/* usage: node index.js markdown_file
-the result will be in autogen.html
+/* Usage: node index.js markdown_file
+   The result will be in the same folder with the same name but with a .html extension
+   Assumes there is a 
 */
